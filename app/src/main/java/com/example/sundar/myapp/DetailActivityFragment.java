81ca939d9f111f392f1a0cce817718c2a21f32f5
sundarsid <sundarspace1@gmail.com>
2016-01-05 12:59:50 +0530

    With share action provider

--- app/src/main/java/com/example/sundar/myapp/DetailActivityFragment.java
+++ app/src/main/java/com/example/sundar/myapp/DetailActivityFragment.java
@@ -3,6 +3,9 @@ package com.example.sundar.myapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.ShareActionProvider;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -15,6 +18,8 @@ import android.widget.TextView;
  * A placeholder fragment containing a simple view.
  */
 public class DetailActivityFragment extends Fragment {
+
+    private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
 
     public DetailActivityFragment() {
     }
@@ -31,6 +36,29 @@ public class DetailActivityFragment extends Fragment {
         // Inflate the menu; this adds items to the action bar if it is present.
         inflater.inflate(R.menu.menu_detail, menu);
 
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+
+        // Get the provider and hold onto it to set/change the share intent.
+        ShareActionProvider mShareActionProvider =
+                (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+
+        // Attach an intent to this ShareActionProvider.  You can update this at any time,
+        // like when the user selects a new piece of data they might like to share.
+        if (mShareActionProvider != null ) {
+            mShareActionProvider.setShareIntent(createShareForecastIntent());
+        } else {
+            Log.d("Hi", "Share Action Provider is null?");
+        }
+
+    }
+
+    private Intent createShareForecastIntent() {
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT,
+                mForecastStr + FORECAST_SHARE_HASHTAG);
+        return shareIntent;
     }
 
     @Override
@@ -42,12 +70,13 @@ public class DetailActivityFragment extends Fragment {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            startActivity(new Intent(getActivity(),SettingsActivity.class));
+            startActivity(new Intent(getActivity(), SettingsActivity.class));
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
+    private String mForecastStr;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -57,9 +86,9 @@ public class DetailActivityFragment extends Fragment {
         // The detail Activity called via intent.  Inspect the intent for forecast data.
         Intent intent = getActivity().getIntent();
         if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
-            String forecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
+            mForecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
             ((TextView) rootView.findViewById(R.id.detail_text))
-                    .setText(forecastStr);
+                    .setText(mForecastStr);
         }
 
         return rootView;
